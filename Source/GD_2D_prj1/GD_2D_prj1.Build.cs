8a6ad53083c0b30b2bbfd1941be1761e097a1270
Kit <23651079@stu.mmu.ac.uk>
2025-02-10 10:52:02 +0000

    Aaaa scarry changing file

--- Source/GD_2D_prj1/GD_2D_prj1.Build.cs
+++ Source/GD_2D_prj1/GD_2D_prj1.Build.cs
@@ -9,5 +9,7 @@ public class GD_2D_prj1 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+
+		EnhancedInput;
 	}
 }
