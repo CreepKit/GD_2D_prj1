f497c92ad2b01351cfc64640dbe3f21dbd91dac7
Kit <23651079@stu.mmu.ac.uk>
2025-03-10 11:43:33 +0000

    changes

--- Source/GD_2D_prj1/Private/BreakableScenery.cpp
+++ Source/GD_2D_prj1/Private/BreakableScenery.cpp
@@ -7,7 +7,9 @@
 ABreakableScenery::ABreakableScenery()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//PrimaryActorTick.bCanEverTick = true;
+	SpriteComponent = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("SpriteComponent")); 
+	RootComponent = SpriteComponent;
 
 }
 
--- Source/GD_2D_prj1/Public/BreakableScenery.h
+++ Source/GD_2D_prj1/Public/BreakableScenery.h
@@ -4,9 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "PaperSpriteComponent.h"
 #include "BreakableScenery.generated.h"
 
-UCLASS()
+//UCLASS()
+
+UCLASS(ClassGroup = "Collision", HideCategories = (Object, LOD, Lighting, TextureStreaming),
+	EditInlineNew, Meta = (DisplayName = "Box Collision", BlueprintSpawnableComponent), MinimalAPI)
+	class UBoxComponent : public UShapeComponent;
+
 class GD_2D_PRJ1_API ABreakableScenery : public AActor
 {
 	GENERATED_BODY()
@@ -15,7 +22,13 @@ public:
 	// Sets default values for this actor's properties
 	ABreakableScenery();
 
+	//Collider
+
 protected:
+	// the objects sprite
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
+	UPaperSpriteComponent* Sprite;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
